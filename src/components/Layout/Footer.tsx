cb919d561341d8c853aa95eb6aeb63f3618807c4
fakhrul-faiz <faiz.now2@gmail.com>
2025-07-15 08:02:43 +0800

    Updated Footer.tsx

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="w-full bg-gray-900 py-4 px-6 text-center">
+    <span className="text-gray-300 text-sm">
+      ©️Copyright 2025&nbsp;|&nbsp;
+      <span className="font-semibold text-white">AZ TITAN LEGACY TRADING</span>
+      &nbsp;|&nbsp;202503107573(003721421)
+    </span>
+  </footer>
+);
+
+export default Footer;
